cecb51b9dcfc1a837969c5b85dd3137d1f014a70
hitheshpoojary <67824540+hitheshpoojary@users.noreply.github.com>
2025-10-08 18:10:09 +0530

    Update PaymentProcessor.java

--- PaymentProcessor.java
+++ PaymentProcessor.java
@@ -1,3 +1,26 @@
 public interface PaymentProcessor {
     void processPayment(double amount);
 }
+
+class PayPalPayment implements PaymentProcessor {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Processing PayPal payment of $" + String.format("%.2f", amount));
+    }
+}
+
+
+class GooglePayPayment implements PaymentProcessor {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Processing GooglePay payment of $" + String.format("%.2f", amount));
+       
+    }
+}
+
+class CreditCardPayment implements PaymentProcessor {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Processing Credit Card payment of $" + String.format("%.2f", amount));
+    }
+}
